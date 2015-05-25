e479cea48a6a53a3854a5053ae02383e76b18a91
edenhub <adamchen.pub@gmail.com>
2015-05-25 13:17:34 +0800

    165

--- 165_CompareVersionNumbers.cpp
+++ 165_CompareVersionNumbers.cpp
@@ -1,89 +1,82 @@
 #include <iostream>
-#include <sstream>
-#include <algorithm>
+#include <stdlib.h>
+#include <vector>
 
 using namespace std;
+
+//1.0.0.0.0 == 1
+//1.0.0.0.1 > 1.0.0.0.0
+//1.2 < 1.10
 
 class Solution {
 public:
     int compareVersion(string version1, string version2) {
-           int po1 = version1.find_first_of('.');
-        int po2 = version2.find_first_of('.');
-        int part1 = helper(version1,version2);
+        vector<int> v1,v2;
+        helper(version1,v1);
+        helper(version2,v2);
 
-        if(part1 != 0) return part1;
+        int s1=0,s2=0;
 
-        version1 = version1.substr(po1+1);
-        version2 = version2.substr(po2+1);
+        while(s1<v1.size() && s2<v2.size()){
+            if(v1[s1] > v2[s2]) return 1;
+            else if(v1[s1] < v2[s2]) return -1;
 
-        return helper(version1,version2);
-
-        // return (version1 < version2) ? -1 :
-        //                     ( (version1 == version2) ? 0 : 1 );
-    }
-
-private:
-    int helper(string version1,string version2){
-        int po1 = version1.find_first_of('.');
-        int po2 = version2.find_first_of('.');
-
-        if(po1 == string::npos && po2 == string::npos){
-            return leftCmp(version1,version2);
+            s1++;
+            s2++;
         }
 
-        if(po1 == string::npos){
-            version2 = version2.substr(0,po2);
-            int ret = leftCmp(version1,version2);
-            if(ret > 0) return 1;
-            else return -1;
+        if(s1 != v1.size()) {
+            while(s1<v1.size()){
+                if(v1[s1++] != 0) return 1;
+            }
+
+            return 0;
         }
+        if(s2 != v2.size()) {
+            while(s2 <v2.size()){
+                if(v2[s2++] != 0) return -1;
+            }
 
-        if(po2 == string::npos){
-            version1 = version1.substr(0,po1);
-            int ret = leftCmp(version1,version2);
-            if(ret < 0) return 1;
-            else return -1;
-        }
-
-
-        if(po1 < po2) return -1;
-
-        if(po1 > po2) return 1;
-
-        string tmp1 = version1.substr(0,po1);
-        string tmp2 = version2.substr(0,po2);
-        if(tmp1 != tmp2){
-            int ret = leftCmp(tmp1,tmp2);
-            if(ret < 0) return -1;
-            else if(ret > 0) return 1;
+            return 0;
         }
 
         return 0;
     }
-    int leftCmp(string version1,string version2){
-        int no1 = version1.find_first_not_of('0');
-        int no2 = version2.find_first_not_of('0');
 
-        if(no1 == string::npos && no2 == string::npos) return 0;
-        else if(no1 == string::npos) return -1;
-        else if(no2 == string::npos) return 1; 
+private:
+    void helper(string version,vector<int>& v){
+        int pre=0,idx=0;
 
-        version1 = version1.substr(no1+1);
-        version2 = version2.substr(no2+1);
+        for(;idx<version.size();idx++){
+            if(version[idx] == '.'){
+                string tmp = version.substr(pre,idx-pre);
+                v.push_back(atoi(tmp.c_str()));
+                pre = idx+1;
+            }
+        }
 
-        return (version1 < version2) ? -1 :
-                            ( (version1 == version2) ? 0 : 1 );
+        string tmp = version.substr(pre);
+        v.push_back(atoi(tmp.c_str()));
     }
 };
 
 int main(){
 
-    string s1="01.1",s2="1.1";
+    string s1="1.0.0.0",s2="1";
 
     Solution s;
+    // vector<int> v1;
+
+    // s.helper(s1,v1);
+    // vector<int>::iterator iter = v1.begin();
+    // while(iter != v1.end())
+    //     cout<<*iter++<<" ";
+    // cout<<endl;
 
     int ret = s.compareVersion(s1,s2);
 
+    // string sss = "012";
+    // cout<<atoi(sss.c_str())<<endl;
     cout<<ret<<endl;
 
     return 0;
